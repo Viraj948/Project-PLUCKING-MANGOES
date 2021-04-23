40b64f8a9123ba5b8b88b16c4a74c0561a7b9eb0
Viraj948 <74658897+Viraj948@users.noreply.github.com>
2021-04-23 19:25:09 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,25 @@
+class Launcher{
+    constructor(bodyA,pointB){
+var options={
+    bodyA:bodyA,
+    pointB:pointB,
+    stiffness:0.004,
+    length:20
+}
+this.pointB=pointB
+this.sling=Constraint.create(options);
+World.add(world,this.sling)
+    }
+    fly(){
+        this.sling.bodyA=null;
+    }
+    
+        Launch(BodyA){
+            this.sling.bodyA=body
+        }
+    
+    display(){
+       
+
+    }
+}
